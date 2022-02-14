f685a945483bd1ff60c54c0aba72d9371c860a26
Alex_Krasko <C:\Users\Admin\AppData\Roaming\The Bat!>
2022-02-14 21:50:44 +0300

    complex & fraction modules added

---
+++ fract_md.py
@@ -0,0 +1,17 @@
+import view
+
+op = view.in_operation()
+operation = {
+    1: lambda x,y: x+y,
+    2: lambda x,y: x-y,
+    3: lambda x,y: x/y,
+    4: lambda x,y: x*y
+    }
+
+def calculation (x,y,oper = operation[op]):
+    return oper(x,y)
+
+def input():
+    x = view.in_fraction('1')
+    y = view.in_fraction('2')
+    return calculation(x,y)
